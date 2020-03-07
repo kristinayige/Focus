be857711d921875c9e093758a0fe88741f6fdcd6
yigel <kristinacy@hotmail.com>
2020-03-06 16:57:25 -0800

    first commit

---
+++ background.js
@@ -0,0 +1,28 @@
+chrome.runtime.onInstalled.addListener(function initialization() {
+    var blockedSites = [];
+    chrome.storage.sync.set({'blockedSites': blockedSites}, function () {});
+});
+
+chrome.tabs.onUpdated.addListener(function blockAction(tabId, info, tab) {
+  chrome.storage.sync.get('blockedSites', function (data) {
+    data.blockedSites.forEach(function (site) {
+      if (tab.url.includes(site)) {
+        chrome.tabs.update(tab.id, { "url": "https://www.nct2018.com/#" });
+      }
+    });
+  });
+});
+
+chrome.contextMenus.create({
+  id: "baFilterListMenu",
+  title: "Show filter list",
+  contexts: ["browser_action"]
+});
+
+chrome.contextMenus.onClicked.addListener(function contextMenuHandler(info, tab) {
+  switch (info.menuItemId) {
+    case "baFilterListMenu":
+      chrome.tabs.create({ url: '/list.html' });
+      break;
+  }
+});
---
+++ temp.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', function renderFilterListTable() {
+    setAddButtonListener();
+});
+
+function setAddButtonListener() {
+    document.getElementById('url').addEventListener("keypress", function (event) {
+        let add = document.getElementById('add');
+        add.addEventListener('click', function () {
+            addToList();
+        });
+        if (event.keyCode == 13) {
+            addToList();
+        }
+    });
+};
+
+function addToList() {
+    let url = document.getElementById('url').value;
+    chrome.storage.sync.get('blockedSites', function (data) {
+        let blacklist = data.blockedSites;
+        blacklist.push(url);
+        chrome.storage.sync.set({ 'blockedSites': blacklist }, function () {
+            document.getElementById('url').value = "";
+            console.log("new url added");
+            chrome.storage.sync.get('blockedSites', function (data) {
+                let urlId = 0;
+                let res = "";
+                let newList = data.blockedSites;
+                newList.forEach(element => {
+                    res += "<tr><td id =\"" + urlId + "\">"
+                        + element + "</td><td><button id =\"b" + urlId
+                        + "\">&times;</button ></td></tr>";
+                    document.getElementById('list').innerHTML = "<table>" + res + "</table>";
+                    urlId = urlId + 1;
+                });
+                document.getElementById('list').innerHTML = "<table>" + res + "</table>";
+                console.log("rendered new table");
+                let btns = document.getElementsByTagName("button");
+                let len = btns.length;
+                for (let i = 0; i < len; i++) {
+                    let element = btns[i];
+                    element.addEventListener("click", function () {
+                        let curId = element.id.substring(1);
+                        console.log("here");
+                        console.log(curId);
+                        // document.getElementById(element.id.substring(1)).innerHTML;
+                        chrome.storage.sync.get('blockedSites', function (data) {
+                            let newBlackList = data.blockedSites;
+                            newBlackList.splice(curId, 1);
+                            chrome.storage.sync.set({ 'blockedSites': newBlackList}, function () {
+                                let newId = 0;
+                                let newRes = "";
+                                newBlackList.forEach(url => {
+                                    newRes += "<tr><td id =\"" + newId + "\">"
+                                        + url + "</td><td><button id =\"b" + newId
+                                        + "\">&times;</button ></td></tr>";
+                                    document.getElementById('list').innerHTML = "<table>" + newRes + "</table>";
+                                    newId = newId + 1;
+                                });
+                                document.getElementById('list').innerHTML = "<table>" + newRes + "</table>";
+                            });
+                        });
+                    });
+                }
+            });
+        });
+    });
+}
+
+
+// chrome.storage.sync.get('blockedSites', function (data) {
+//     blockedSites = data.blockedSites;
+// });
+
+// chrome.webRequest.onBeforeRequest.addListener(
+//     page => {
+//         console.log('page blocked - ' + page.url);
+
+//         return {
+//           cancel: true,
+//         };
+//       }, {urls: ["http://www.youtube.com"]}, ["blocking"]);
+
+// function requestChecker(request) {
+//     console.log("onBeforeRequest");
+//     if (request && request.url) {
+//       if (request.type == "main_frame") {
+//         var tabBlockingState = 0;
+//         for (var i = 0; i < blockedSites.length; ++i) {
+//           if (request.url.match(new RegExp(
+//               ".*" + blockedSites[i] + ".*", "i"))) {
+//             tabBlockingState = blockedSites[i];
+//           }
+//         }
+//         if (tabBlockingState != 0) {
+//           var redirectUrl = chrome.extension.getURL(
+//               "blockedSite.html?blocked=" + tabBlockingState);
+//           return { redirectUrl: redirectUrl };
+//         }
+//       }
+//     }
+//   }
