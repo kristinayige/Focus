efe76c4855582b9bd57afabab9a655ee6d42ab26
yigel <kristinacy@hotmail.com>
2020-03-10 12:00:01 -0400

    added background

--- background.js
+++ background.js
@@ -1,49 +1,51 @@
 chrome.runtime.onInstalled.addListener(function initialization() {
-    var blockedSites = [];
-    chrome.storage.sync.set({'blockedSites': blockedSites, 'isActivated':true}, function () {});
+  var blockedSites = [];
+  chrome.storage.sync.set({ 'blockedSites': blockedSites, 'isActivated': true }, function () { });
 });
 
 chrome.tabs.onUpdated.addListener(function blockAction(tabId, info, tab) {
-  chrome.storage.sync.get(['blockedSites', 'isActivated'], function (data) {
-    if(data.isActivated==true){
-      data.blockedSites.forEach(function (site) {
-        if (tab.url.includes(site)) {
-          chrome.tabs.update(tab.id, { "url": "/blocked.html" });
-        }
-      });
+  chrome.storage.sync.get(['blockedSites', 'isActivated', 'block_mode_up'], function (data) {
+    if (data.block_mode_up == true) {
+      if (data.isActivated == true) {
+        data.blockedSites.forEach(function (site) {
+          if (tab.url.includes(site)) {
+            chrome.tabs.update(tab.id, { "url": "/blocked.html" });
+          }
+        });
+      }
     }
   });
 });
-chrome.storage.onChanged.addListener(function(changes) {
-    for(let key in changes){
-      if(key === 'isActivated' &&changes['isActivated'].newValue == false &&changes['isActivated'].oldValue == true ){
-        chrome.storage.sync.get('timer',function(data){
-          function Reblock() {
-              let r = confirm("Block Again?");
-              if(r==true){
-                chrome.storage.sync.set({'isActivated':true},function(){
-                  chrome.storage.sync.get('blockedSites', function (data) {
-                      data.blockedSites.forEach(function (site) {
-                        chrome.tabs.query({}, function(tabs){
-                          tabs.forEach((item, i) => {
-                            if (item.url.includes(site)) {
-                              chrome.tabs.update(item.id, { "url": "/blocked.html" });
-                            }
-                          });
-                        })
-                      });
-                  });
+chrome.storage.onChanged.addListener(function (changes) {
+  for (let key in changes) {
+    if (key === 'isActivated' && changes['isActivated'].newValue == false && changes['isActivated'].oldValue == true) {
+      chrome.storage.sync.get('timer', function (data) {
+        function Reblock() {
+          let r = confirm("Block Again?");
+          if (r == true) {
+            chrome.storage.sync.set({ 'isActivated': true }, function () {
+              chrome.storage.sync.get('blockedSites', function (data) {
+                data.blockedSites.forEach(function (site) {
+                  chrome.tabs.query({}, function (tabs) {
+                    tabs.forEach((item, i) => {
+                      if (item.url.includes(site)) {
+                        chrome.tabs.update(item.id, { "url": "/blocked.html" });
+                      }
+                    });
+                  })
                 });
-                alert("Blocked Again!");
-              }else{
-                setTimeout(Reblock,data.timer*1000);
-              }
+              });
+            });
+            alert("Blocked Again!");
+          } else {
+            setTimeout(Reblock, data.timer * 1000);
           }
-          setTimeout(Reblock,data.timer*1000);
-        });
-        break;
-      }
+        }
+        setTimeout(Reblock, data.timer * 1000);
+      });
+      break;
     }
+  }
 });
 
 chrome.contextMenus.create({
