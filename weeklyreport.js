dee556e53d65eef465ce2c5c2779316b5bf465a4
mwdgdx <mwdgdx@umich.edu>
2020-04-18 22:30:22 -0400

    sdf

--- weeklyreport.js
+++ weeklyreport.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function renderFilterListTable() {
       document.getElementById("improvements").innerHTML=Math.ceil((data.thisWeek-data.lastWeek)/60000);
       document.getElementById("blockedTimes").innerHTML=data.blockedTimes;
       document.getElementById("cumuBlockTimes").innerHTML=data.cumuBlockTimes;
-
+      let str = "This week I saved "+ Math.ceil(data.thisWeek/60000) +" minutes by FOCUS!!";
+      document.getElementById("twitter").href = "https://twitter.com/intent/tweet?text="+str;
+      document.getElementById("linkedin").href = "https://www.linkedin.com/shareArticle?mini=true&url=focus.com&title=FOCUS!&summary="+ str+"&source=";
     })
+
+
 });
